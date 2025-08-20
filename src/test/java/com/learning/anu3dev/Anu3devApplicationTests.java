fba32430c5eea6a99d9489940c705146e9dea40e
anurag git <anurag@git>
2025-08-20 18:09:11 -0500

    initial setup

---
+++ src/test/java/com/learning/anu3dev/Anu3devApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.anu3dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anu3devApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
